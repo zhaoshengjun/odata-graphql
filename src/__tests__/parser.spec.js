0b6fe35eee0237f7723666fc50fe494d66effb82
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-05 15:42:35 +1000

    parse normal node

--- src/__tests__/parser.spec.js
+++ src/__tests__/parser.spec.js
@@ -1,11 +1,23 @@
 const parse = require('../parser').parse;
+const root = { children: [] };
 describe('parser', () => {
 	it('should parse the definition node', () => {
 		const xml = '<?xml version="1.0" encoding="utf-8"?>';
-		let node = parse(xml, null);
+		let node = parse(xml, root);
 		expect(node.name).toBe('xml');
 		expect(node.type).toBe('XMLDefinition');
 		expect(node.attributes).toContainEqual({ name: 'version', value: '1.0' });
 		expect(node.attributes).toContainEqual({ name: 'encoding', value: 'utf-8' });
+	});
+
+	it('should parse normal node', () => {
+		const xml = `<Principal Role="FromRole_ZDIS_CIRC_CLU_To_M_ZTDL_MED_ASS">`;
+		let node = parse(xml, root);
+		expect(node.type).toBe('XMLNode');
+		expect(node.name).toBe('Principal');
+		expect(node.attributes).toContainEqual({
+			name: 'Role',
+			value: 'FromRole_ZDIS_CIRC_CLU_To_M_ZTDL_MED_ASS'
+		});
 	});
 });
--- src/parser.js
+++ src/parser.js
@@ -9,9 +9,11 @@ const normalized = name => name;
  * @param {XMLNode} rootNode
  */
 const parse = (chunk, rootNode) => {
+	// REMEMBER: the chunk always ends with ">"
 	let cleanChunk = chunk.replace(/^\s*|\s*$/g, '').replace(/\s{2,}/g, ' '); // clean space
 	let node;
 	let attributes = [];
+	// definition node
 	if (cleanChunk.startsWith('<?') && cleanChunk.endsWith('?>')) {
 		cleanChunk = cleanChunk.replace('<?', '').replace('?>', '');
 		// definition node
@@ -32,10 +34,48 @@ const parse = (chunk, rootNode) => {
 			value: chunk,
 			parent: rootNode,
 			attributes,
-			children: null
+			children: []
 		});
+		rootNode.children.push(node);
+		return node;
 	}
-	return node;
+	// close a node
+	if (cleanChunk.startsWith('</')) {
+		// no meaning for creating node for this
+		//  only needs to do error-checking
+		// so just return the parent node (like pop operation)
+		if (rootNode.parent) {
+			return rootNode.parent;
+		} else {
+			return null;
+		}
+	}
+	// start a new node
+	if (cleanChunk.startsWith('<')) {
+		cleanChunk = cleanChunk.replace('<', '').replace('>', '');
+
+		let groups = cleanChunk.split(' ');
+		groups.forEach(group => {
+			if (group.includes('=')) {
+				attributes.push({
+					name: group.split('=')[0],
+					value: group.split('=')[1].replace(/\"/g, '')
+				});
+			} else {
+				name = group;
+			}
+		});
+		node = createXMLNode({
+			type: 'XMLNode',
+			name: normalized(name),
+			value: chunk,
+			parent: rootNode,
+			attributes,
+			children: []
+		});
+		rootNode.children.push(node);
+		return node;
+	}
 };
 
 module.exports = { parse };
