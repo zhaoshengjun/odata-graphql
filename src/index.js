cd4bb957fe8353ad0aa9edf854f94539f36e1dec
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-10 09:50:40 +1000

    refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const { map, reduce } = require('rxjs/operators');
-const readChunk = require('./file').readChunk;
+const { map } = require('rxjs/operators');
+
 const createXMLNode = require('./xml').createXMLNode;
 const parse = require('./parser').parse;
 const fromStream = require('./fromStream');
@@ -35,22 +35,19 @@ fromStream(newStream)
 	.subscribe(
 		node => {
 			console.log('[data]', node);
-			let clone = Object.assign({}, node);
-			nodes.push(clone);
-			// console.log('[chunk]', decoder.write(data) + '>');
-			// const chunk = decoder.write(data) + '>';
-			// const node = parse(chunk);
-			// // console.log('[current node]', node);
-			// if (node) {
-			// 	level = level + 1;
-			// 	node['level'] = level;
-			// 	nodes = [...nodes, node];
-			// 	if (node.selfClosing) {
-			// 		level = level - 1;
-			// 	}
-			// } else {
-			// 	level = level - 1;
-			// }
+			let cloneNode = Object.assign({}, node);
+			if (cloneNode) {
+				// add level info
+				level = level + 1;
+				cloneNode['level'] = level;
+				nodes.push(cloneNode);
+
+				if (cloneNode.selfClosing) {
+					level = level - 1;
+				}
+			} else {
+				level = level - 1;
+			}
 		},
 		err => console.log(err),
 		() => {
@@ -58,51 +55,3 @@ fromStream(newStream)
 			console.log('nodes:', nodes);
 		}
 	);
-// parse the chunk and generate AST
-
-// let readChunkPromise = promisify(readChunk);
-// readChunkPromise(file, {})
-// 	.then(chunk => {
-// 		console.log(chunk);
-// 	})
-// 	.catch(err => console.log(err));
-// readChunk(
-// 	file,
-// 	{},
-// 	{
-// 		onData: chunk => {
-// 			// console.log('[chunk]', chunk);
-// 			// 2. parse chunk
-// 			nodes = [...nodes, parse(chunk)];
-// 			const node = parse(chunk);
-// 			debugger;
-// 			// for closing tag, no node will be returned
-// 			// console.log('[current node]-[name]', changable.currentNode.name);
-// 			// console.log('[current node]-[type]', changable.currentNode.type);
-// 			if (node) {
-// 				level = level + 1;
-// 				node['level'] = level;
-// 				// nodes.push(node);
-// 				if (node.selfClosing) {
-// 					level = level - 1;
-// 				}
-// 				// console.log('[node]', node);
-// 				// console.log('[current node]-[attributes]', node.attributes);
-// 				// create relationship
-// 				// currentNode.children.push(node);
-// 				// node.parent = currentNode;
-// 				// only not closing tag will be pushed into the AST stack
-// 				// if (node.type === 'XMLNode' && !node.selfClosing) {
-// 				// 	changable.currentNode = node;
-// 				// }
-// 			} else {
-// 				// closing tag
-// 				level = level - 1;
-// 			}
-// 		},
-// 		onFinish: () => {
-// 			console.log('done!');
-// 			console.log('[AST]:', nodes);
-// 		}
-// 	}
-// );
