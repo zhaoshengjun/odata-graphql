6d325462e8de5fee8403cfea2084db156b23e2c3
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-10 11:02:43 +1000

    generate AST

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+const fs = require('fs');
 const { map } = require('rxjs/operators');
-
 const createXMLNode = require('./xml').createXMLNode;
 const parse = require('./parser').parse;
 const fromStream = require('./fromStream');
@@ -10,19 +10,23 @@ const delimiterStream = new DelimiterStream({
 });
 const decoder = new StringDecoder('utf8');
 const file = './test/ZCLK_DISABILITY_CIRC_CLUSTER_SRV.xml';
-// 1. create root node for xml
-let xmlFile = createXMLNode({
-	type: 'XMLFile',
-	name: file, // should use normalized file name
-	value: '',
-	parent: null,
-	attributes: null,
-	children: []
-});
-let nodes = [];
-let level = 1;
 
-const fs = require('fs');
+// 1. create root node for xml
+const createRootNode = () => {
+	return createXMLNode({
+		type: 'XMLFile',
+		name: file, // should use normalized file name
+		value: '',
+		parent: null,
+		attributes: null,
+		children: [],
+		level: 1
+	});
+};
+
+let level = 1;
+let nodes = [];
+nodes.push(createRootNode());
 
 const readStream = fs.createReadStream(file);
 const newStream = readStream.pipe(delimiterStream);
@@ -34,9 +38,10 @@ fromStream(newStream)
 	)
 	.subscribe(
 		node => {
-			console.log('[data]', node);
-			let cloneNode = Object.assign({}, node);
-			if (cloneNode) {
+			// console.log('[data]', node);
+			// debugger;
+			if (node) {
+				let cloneNode = Object.assign({}, node);
 				// add level info
 				level = level + 1;
 				cloneNode['level'] = level;
@@ -51,7 +56,33 @@ fromStream(newStream)
 		},
 		err => console.log(err),
 		() => {
-			console.log('done');
-			console.log('nodes:', nodes);
+			// generate AST
+			generateAST(nodes);
+			// parse AST to generate graphql schema
 		}
 	);
+
+// convert flat nodes into tree shape
+// the nodes should be ordered (as the same order in the source file)
+const generateAST = nodes => {
+	let prevNode = nodes[0]; // root
+	let tree = nodes[0];
+	for (let index = 1; index < nodes.length; index++) {
+		const node = nodes[index];
+		if (node.level > prevNode.level) {
+			// child node
+			prevNode.children.push(node);
+			node.parent = prevNode;
+		} else if (node.level < prevNode.level) {
+			// parent's sibling node
+			prevNode.parent.parent.children.push(node);
+			node.parent = prevNode.parent.parent;
+		} else {
+			// sibling node
+			prevNode.parent.children.push(node);
+			node.parent = prevNode.parent;
+		}
+		prevNode = node;
+	}
+	return tree;
+};
